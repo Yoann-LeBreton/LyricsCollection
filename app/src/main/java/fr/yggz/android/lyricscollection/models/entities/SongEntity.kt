6f7d900fe98af95f16440f5d683797ebe153727a
Yoann Le Breton <yoann.lebreton@niji.fr>
2022-03-25 19:41:41 +0100

    set first class

---
+++ app/src/main/java/fr/yggz/android/lyricscollection/models/entities/SongEntity.kt
@@ -0,0 +1,9 @@
+package fr.yggz.android.lyricscollection.models.entities
+
+data class SongEntity(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String,
+)
